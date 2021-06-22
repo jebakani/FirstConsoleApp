18cb8cf5ef63103fb2841d219cd48c66d8829806
Jebakani <vjishwarya@gmail.com>
2021-06-22 17:15:22 +0530

    Add project files.

---
+++ FirstConsoleApp/Operation.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FirstConsoleApp
+{
+    class Operation
+    {
+       public int Multiplication(int var1,int var2)
+        {
+            return var1 * var2;
+        }
+    }
+}
---
+++ FirstConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation operation = new Operation();
+
+            string varialbleOne = "My first Variable=";
+            int variableTwo = 1;
+            int variableThree = 5;
+            int result = Addition(variableTwo, variableThree);
+            Console.WriteLine("Addition:" + result);
+            Console.WriteLine("Multiplication in class:" + operation.Multiplication(5, 3));
+            Console.WriteLine(varialbleOne + variableTwo);
+            Console.WriteLine("Hello World!");
+        }
+        private static int Addition(int varOne, int varTwo)
+        {
+            return varOne + varTwo;
+        }
+    }
+}
+  
